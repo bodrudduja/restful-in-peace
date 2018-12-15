76ad0caac6bedf7ceb18c0fc4ea654f0e5b81045
bodrudduja <bodruddujachowdhury1994@gmail.com>
2018-12-15 14:08:07 +0600

    First commmit

---
+++ src/main/java/com/rokomari/ResTfulInPeaceApplication.java
@@ -0,0 +1,19 @@
+package com.rokomari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@SpringBootApplication
+@EnableJpaRepositories
+@ComponentScan
+@EnableAutoConfiguration
+public class ResTfulInPeaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulInPeaceApplication.class, args);
+	}
+
+}
+
